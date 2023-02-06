3ef9927d6b0e95209b58c60228b7dbc15e9faeb6
Thor Kamphefner <thorck@pm.me>
2023-02-05 16:59:34 -0800

    poke

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import TableInputs from './components/TableInputs';
+import TableEquation from './components/TableEquation';
+import Table from './components/Table';
+
+
+export interface Inputs {
+  p: number, a: number, b: number
+}
+
+const emptyInputs: Inputs = {
+  p: 7,
+  a: 0,
+  b: 2
+}
+
 function App() {
+  const [inputs, setInputs] = useState(emptyInputs)
+  // don't want to change state beneath where state is kept
+  function handleChangeP(event: React.ChangeEvent<HTMLInputElement>) {
+    // call setInputs with new state
+    setInputs({ ...inputs, p: event.target.valueAsNumber })
+    // react change events parameterized for HTMLChangeInptu
+
+    // setInputs({ ...inputs, p: event.target.value })
+  }
+  function handleChangeA(event: React.ChangeEvent<HTMLInputElement>) {
+    setInputs({ ...inputs, a: parseInt(event.target.value) })
+    // setInputs({ ...inputs, a: event.target.value })
+  }
+  function handleChangeB(event: React.ChangeEvent<HTMLInputElement>) {
+    setInputs({ ...inputs, b: parseInt(event.target.value) })
+    // setInputs({ ...inputs, b: event.target.value })
+  }
   return (
     <div className="App">
-      <header className="App-header">
+      <TableInputs
+        // pass them proppppps
+        inputs={inputs}
+        handleChangeP={handleChangeP}
+        handleChangeA={handleChangeA}
+        handleChangeB={handleChangeB}
+      />
+      <TableEquation inputs={inputs}      // lets GO
+      
+      />
+      <Table />
+
+
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,7 +63,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
