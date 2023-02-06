c86ed32190158aca116158723e78a81b9150bce6
Thor Kamphefner <thorck@pm.me>
2023-02-05 18:52:35 -0800

    latex

--- src/components/TableEquation.tsx
+++ src/components/TableEquation.tsx
@@ -1,6 +1,8 @@
 import react from 'react';
 import { Inputs } from '../App';
-
+// import {InlineMath } from 'react-katex'
+import 'katex/dist/katex.min.css';
+import { InlineMath, BlockMath } from 'react-katex';
 
 interface Props {
     inputs: Inputs,
@@ -10,8 +12,8 @@ interface Props {
 export default function TableEquation(props: Props) {
     const { inputs } = props
     return (
-        <div>
-            y^2 = x^3
+        <div className='equation'>
+          <InlineMath math="y^2=x^3"/>
             {inputs.a === 0 ? " " : `+ ${inputs.a}x `}
             {inputs.b === 0 ? " " : `+ ${inputs.b} `}
             (mod {inputs.p}) over F{inputs.p} with some number of points
